89d0cd4fc1a96b42d877eb7314362330e1d2ce07
rahuldahal <rdaahal@gmail.com>
2023-05-30 18:55:20 +0545

    chore: use morgan HTTP logger

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,19 @@
+import morgan from 'morgan';
+import dotenv from 'dotenv';
 import express from 'express';
+import routes from './routes';
 import logger from './utils/logger';
 import connect from './utils/connect';
-import dotenv from 'dotenv';
-import routes from './routes';
+
 dotenv.config();
 
 const PORT = process.env.PORT;
 
 const app = express();
 app.use(express.json());
+
+// morgan http logger
+app.use(morgan('tiny'));
 
 app.listen(PORT, async () => {
   logger.info(`App is running at http://localhost:${PORT}`);
