e7bf18328e49c482a8b966e118cbb598f81f5170
rahuldahal <rdaahal@gmail.com>
2023-06-08 09:14:30 +0545

    refactor: use morgan log on app

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+import morgan from 'morgan';
 import dotenv from 'dotenv';
 import logger from './utils/logger';
 import connect from './utils/connect';
@@ -8,6 +9,8 @@ dotenv.config();
 const PORT = process.env.PORT;
 
 const app = createServer();
+// morgan http logger
+app.use(morgan('tiny'));
 
 app.listen(PORT, async () => {
   logger.info(`App is running at http://localhost:${PORT}`);
--- src/utils/server.ts
+++ src/utils/server.ts
@@ -1,13 +1,10 @@
-import morgan from 'morgan';
 import express from 'express';
 import routes from '../routes';
 
 export default function createServer() {
   const app = express();
-  app.use(express.json());
 
-  // morgan http logger
-  app.use(morgan('tiny'));
+  app.use(express.json());
   routes(app);
 
   return app;
